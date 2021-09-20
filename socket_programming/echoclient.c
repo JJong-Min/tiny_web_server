7e1b2bdcff8fad3f3e49bef47b813a7f9581e325
JJong-Min <sambujakkk@naver.com>
2021-09-20 15:59:43 +0900

    complete echoclient.c

--- socket_programming/echoclient.c
+++ socket_programming/echoclient.c
@@ -1,138 +1,5 @@
 #include "csapp.h"
 
-void rio_readinitb(rio_t *rp, int fd)
-{
-    rp->rio_fd = fd;
-    rp->rio_cnt = 0;
-    rp->rio_bufptr = rp->rio_buf;
-}
-
-static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
-{
-    int cnt;
-
-    while (rp->rio_cnt <= 0) { /* Refill if buf isempty */
-        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
-
-        sizeof(rp->rio_buf));
-        if (rp->rio_cnt < 0) {
-            if (errno != EINTR) /*Interrupted by sig handler return */
-                return -1;
-        }
-        else if (rp->rio_cnt == 0) /* EOF */
-            return 0;
-        else
-            rp->rio_bufptr = rp->rio_buf; /*
-        Reset buffer ptr */
-    }
-
-    /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
-    cnt = n;
-    if (rp->rio_cnt < n)
-        cnt = rp->rio_cnt;
-    memcpy(usrbuf, rp->rio_bufptr, cnt);
-    rp->rio_bufptr += cnt;
-    rp->rio_cnt -= cnt;
-    return cnt;
-}
-
-ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
-{
-    int n, rc;
-    char c, *bufp = usrbuf;
-
-    for (n = 1; n < maxlen; n++) {
-        if ((rc = rio_read(rp, &c, 1)) == 1) {
-            *bufp++ = c;
-            if (c == '\n') {
-                n++;
-                break;
-        }
-        } else if (rc == 0) {
-            if (n == 1)
-            return 0; /* EOF, no data read */
-            else 
-            break; /* EOF, some data was read */
-        } else
-            return -1; /* Error*/
-    }
-    *bufp = 0;
-    return n-1;
-}
-
-ssize_t rio_readn(int fd, void *usrbuf, size_t n)
-{
-    size_t nleft = n; 
-    ssize_t nread;
-    char *bufp = usrbuf;
-
-    while (nleft > 0) {
-        if ((nread = read(fd, bufp, nleft)) < 0)
-        {
-            if (errno == EINTR) /*Interrupted by sig handler return */
-                nread = 0;      /* and call read() again */
-            else
-                return -1; /* errno set by read() */
-            }
-            else if (nread == 0)
-                break; /*EOF */
-        nleft -= nread;
-        bufp += nread;
-    }
-    return (n - nleft); /* Return >= 0 */
-}
-
-ssize_t rio_writen(int fd, void *usrbuf, size_t n)
-{
-    size_t nleft = n;
-    ssize_t nwritten;
-    char *bufp = usrbuf;
-
-    while (nleft > 0) {
-        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
-            if (errno == EINTR) /*Interrupted by sig handler return */
-                nwritten = 0; /* and call write() again */
-            else
-                return -1; /* errno set by write() */
-        }
-        nleft -= nwritten;
-        bufp += nwritten;
-    }
-    return n;
-}
-
-
-int open_clientfd(char *hostname, char *port) {
-    int clientfd;
-    struct addrinfo hints, *listp, *p;
-
-    /* Get a list of potential server addresses */
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_socktype = SOCK_STREAM; /* Open a connection */
-    hints.ai_flags = AI_NUMERICSERV; /* ... using a numeric port arg. */
-    hints.ai_flags |= AI_ADDRCONFIG; /* Recommended for connections */
-    getaddrinfo(hostname, port, &hints, &listp);
-
-    /* Walk the list for one that we can successfully connect to */
-    for (p = listp; p; p = p->ai_next) {
-    /* Create a socket descriptor */
-        if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
-            continue; /* Socket failed, try the next */
-
-        /* Connect to the server */
-        if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
-        break; /* Success */
-        close(clientfd); /* Connect failed, try another */
-    }
-
-    /* Clean up */
-    freeaddrinfo(listp);
-    if (!p) /* All connects failed */
-    return -1;
-    else /* The last connect succeeded */
-    return clientfd;
-}
-
 int main(int argc, char **argv)
 {
     int clientfd;
@@ -147,13 +14,13 @@ int main(int argc, char **argv)
     port = argv[2];
 
     clientfd = open_clientfd(host, port);
-    rio_readinitb(&rio, clientfd);
+    Rio_readinitb(&rio, clientfd);
 
     while (fgets(buf, MAXLINE, stdin) != NULL) {
-        rio_writen(clientfd, buf, strlen(buf));
-        rio_readlineb(&rio, buf, MAXLINE);
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
         fputs(buf, stdout);
     }
-    close(clientfd);
+    Close(clientfd);
     exit(0);
 }
